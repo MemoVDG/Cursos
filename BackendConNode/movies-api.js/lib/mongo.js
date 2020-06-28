35e968f214a172850265b78952a1728644665b1f
Memo Vara <memovdg@gmail.com>
2020-06-28 18:37:07 -0500

    Mongo

--- BackendConNode/movies-api.js/lib/mongo.js
+++ BackendConNode/movies-api.js/lib/mongo.js
@@ -1,53 +1,52 @@
-const { MogoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const { config } = require('../config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const DB_NAME = config.dbHost;
+const DB_NAME = config.dbName;
 
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
 
-class Mongolib {
+class MongoLib {
   constructor() {
-    this.client = new MogoClient(MONGO_URI, { useNewUrlParser: true });
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
     this.dbName = DB_NAME;
   }
 
   connect() {
-    if (!Mongolib.connection) {
-      Mongolib.connection = new Promise((resolve, reject) => {
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
         this.client.connect((err) => {
           if (err) {
             reject(err);
           }
 
-          console.log('Connected success');
+          console.log('Connected succesfully to mongo');
+          console.log(this.client);
           resolve(this.client.db(this.dbName));
         });
       });
     }
 
-    return Mongolib.connection;
+    return MongoLib.connection;
   }
 
   getAll(collection, query) {
     return this.connect().then((db) => {
-      return db.collection().find(query).toArray();
+      return db.collection(collection).find(query).toArray();
     });
   }
 
   get(collection, id) {
-    return this.connect()
-      .then((db) => {
-        return db.collection.findOne({ _id: ObjectId(id) });
-      })
-      .findOne({ _id: ObjectId(id) });
+    return this.connect().then((db) => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
   }
 
   create(collection, data) {
     return this.connect()
       .then((db) => {
-        return db.collection.insertOne(data);
+        return db.collection(collection).insertOne(data);
       })
       .then((result) => result.insertedId);
   }
@@ -55,11 +54,9 @@ class Mongolib {
   update(collection, id, data) {
     return this.connect()
       .then((db) => {
-        return db.collection.updateOne(
-          { _id: ObjectId(id) },
-          { $set: data },
-          { upsert: true }
-        );
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
       })
       .then((result) => result.upsertedId || id);
   }
@@ -67,10 +64,10 @@ class Mongolib {
   delete(collection, id) {
     return this.connect()
       .then((db) => {
-        return db.collection.deleteOne({ _id: ObjectId(id) });
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
       })
       .then(() => id);
   }
 }
 
-module.exports = Mongolib;
+module.exports = MongoLib;
