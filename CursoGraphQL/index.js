81767da5c297650e916042754c255b7ca3c68976
MemoVDG <memovdg@gmail.com>
2020-08-11 16:28:25 -0500

    Configuracion basica

--- CursoGraphQL/index.js
+++ CursoGraphQL/index.js
@@ -1,32 +1,18 @@
 'use strict'
-const { buildSchema } = require('graphql');
+const { makeExecutableSchema  } = require('graphql-tools');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { readFileSync } = require('fs');
 const { join } = require('path');
+const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.port || 3000;
 
 // Definiendo schema
-const schema = buildSchema(
-    readFileSync(
-        join(
-            __dirname, 
-            'lib', 
-            'schema.graphql'
-            ),
-            'utf-8'
-            ));
+const typeDefs = readFileSync( join( __dirname, 'lib', 'schema.graphql'), 'utf-8');
+const schema = makeExecutableSchema({typeDefs, resolvers});
 
-// Configuracion de resolvers
-const resolvers = {
-    // Por cada query tenemos que crear el resolver
-    hello: () => {
-        return 'Hola Mundo'
-    }
-
-}
 
 app.use('/api', graphqlHTTP({
     schema: schema,
