f4f2700c10afd79a1d3ea7380956e3cedb8f307b
MemoVDG <memovdg@gmail.com>
2020-08-12 11:13:41 -0500

    Conexion a mongo atlas

---
+++ CursoGraphQL/lib/db.js
@@ -0,0 +1,38 @@
+'use strict'
+const { MongoClient } = require('mongodb');
+
+const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME
+} = process.env
+
+
+const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
+let connection;
+
+async function connectDB(){
+    if(connection){
+        return connection
+    }
+
+    let  client;
+    try {
+        client = await MongoClient.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        connection = client.db(DB_NAME);
+    } catch (error) {
+        console.error('Couldt connecto to the db', MONGO_URI, error);
+        process.exit(1)
+    }
+
+    return connection;
+}
+
+module.exports = connectDB;
