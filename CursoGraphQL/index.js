cb711ea75a518a7ebbde8e1f96fc07dd251cb626
MemoVDG <memovdg@gmail.com>
2020-08-13 17:22:55 -0500

    Produccion

--- CursoGraphQL/index.js
+++ CursoGraphQL/index.js
@@ -6,6 +6,7 @@ const { graphqlHTTP } = require('express-graphql');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const resolvers = require('./lib/resolvers');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -13,12 +14,17 @@ const port = process.env.port || 3000;
 // Definiendo schema
 const typeDefs = readFileSync( join( __dirname, 'lib', 'schema.graphql'), 'utf-8');
 const schema = makeExecutableSchema({typeDefs, resolvers});
+// Varificamos si estamos en produccion para mostrar o no graphiql
+const isDev = process.env.NODE_ENV.trimRight() !== 'production';
 
+
+// Configuracion de CORS
+app.use(cors());
 
 app.use('/api', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
 }))
 
 app.listen(port, () => {
