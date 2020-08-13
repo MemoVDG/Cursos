0fdfb652bbf1712b59cd6d619f774a75c6dd7e03
MemoVDG <memovdg@gmail.com>
2020-08-13 13:07:05 -0500

    Manejo de errores e interfaces

--- CursoGraphQL/lib/queries.js
+++ CursoGraphQL/lib/queries.js
@@ -3,6 +3,55 @@
 const connectDb = require('./db');
 const { ObjectID } = require('mongodb')
 
+/*
+    Ejemplo query basica
+    {
+        gerCourses(){
+            _id
+            title
+        }
+    }
+    Query con parametros
+    {
+        getCourse(_id: "hjsdgdh526"){
+            title
+        }
+    }
+
+    // ALIAS, sirven para traer mas de una query en una sola peticion
+    {
+        AllCourses: getCourses{
+            _id
+            title
+        }
+
+        AllStudents: getStudents{
+            _id
+            name
+        }
+    }
+
+    // Fragments, sirve para reocupar secciones ya traidas en una peticion y ocupar esos campos en otra peticion
+    {
+        AllCourses: getCourses{
+            ...CourseFields
+            description
+        }
+
+        Course1: getCourse(_id: "kjha7978jkhsdk"){
+            ...CourseFields
+            teacher
+            topic
+        }
+
+    }
+
+    fragment CourseFields on Course{
+        _id
+        title
+    }
+*/
+
 module.exports = {
     getCourses: async () => {
         let db;
@@ -15,20 +64,20 @@ module.exports = {
         }
         return courses
     },
-    getCourse: async (root, { id }) => {
+    getCourse: async (root, { _id }) => {
         let db;
         let course;
         try {
             db = await connectDb();
-            course = await db.collection('courses').findOne({_id : ObjectID(id) });
+            course = await db.collection('courses').findOne({_id : ObjectID(_id) });
         } catch(error) {
             console.error(error);
         }
         return course;
     },
 
-    // Students
-    getStudents: async () => {
+    // People
+    getPeople: async () => {
         let db;
         let students = [];
         try {
@@ -39,12 +88,12 @@ module.exports = {
         }
         return students
     },
-    getStudent: async (root, { id }) => {
+    getPerson: async (root, { _id }) => {
         let db;
         let student;
         try {
             db = await connectDb();
-            student = await db.collection('students').findOne({_id : ObjectID(id) });
+            student = await db.collection('students').findOne({_id : ObjectID(_id) });
         } catch(error) {
             console.error(error);
         }
