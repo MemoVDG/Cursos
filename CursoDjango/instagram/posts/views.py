0940e6414817138a8cade1c8387b2ad8a413ff4b
MemoVDG28 <memo@plataforma.io>
2020-09-09 21:42:26 -0500

    Intial commit

---
+++ CursoDjango/instagram/posts/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+
+
+# Utilities
+from datetime import datetime
+
+posts = [
+    {
+        'name': 'Mont Blanc',
+        'user': 'Yesica Cortes',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'http://picsum.photos/200/200?image=1036'
+    },
+    {
+        'name': 'Lee Blanc',
+        'user': 'Raul Cortes',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'http://picsum.photos/200/200?image=136'
+    },
+    {
+        'name': 'Mont Ruiz',
+        'user': 'Mica Rues',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'http://picsum.photos/200/200?image=103'
+    }
+]
+
+def list_post(request):
+    """List existing posts"""
+
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+
+    return HttpResponse('<br>'.join(content))
