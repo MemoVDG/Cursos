218372bfa740ec586cc804a6674b2ab81c887d77
Memo28 <memovdg@gmail.com>
2020-09-24 20:00:37 -0500

    [Add] Validacion con formulatios

--- CursoDjango/instagram/users/views.py
+++ CursoDjango/instagram/users/views.py
@@ -9,12 +9,32 @@ from users.models import Profile
 # Exceptions
 from django.db.utils import IntegrityError
 
+#Form
+from users.forms import ProfileForm
+
 def update_profile(request):
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
     return render(
         request,
-        'users/update_profile.html'
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
     )
-
 
 def login_view(request):
     if request.method == 'POST':
